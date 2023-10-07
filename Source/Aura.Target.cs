08c85006dd1edf5ac38c1832a6853d9b34922905
Anders <oakvalley.a@gmail.com>
2023-10-07 21:50:20 +0200

    Added characterbase class

---
+++ Source/Aura.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Anders Ekdahl
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class AuraTarget : TargetRules
+{
+	public AuraTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+
+		ExtraModuleNames.AddRange( new string[] { "Aura" } );
+	}
+}
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Anders Ekdahl
+
+#pragma once
+
+#include "CoreMinimal.h"
+
